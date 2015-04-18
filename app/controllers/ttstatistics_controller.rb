5849a0258f5f1667ee98fe2f06105a8ed6ab48e7
seventhsense <seventh@scimpr.com>
2015-04-19 08:48:37 +0900

    add feature devide statistics page

--- app/controllers/ttstatistics_controller.rb
+++ app/controllers/ttstatistics_controller.rb
@@ -1,12 +1,23 @@
 class TtstatisticsController < ApplicationController
   # unloadable
+  before_action :set_unreported_ttevents_count, only: [:index, :stats_by_month, :stats_by_day]
 
   def index
     @unreported_ttevents = Ttevent.where('is_done = ? AND end_time < ?',false, Time.now).count
+  end
+
+  def stats_by_month
     @ttevents_by_month = Ttevent.where(is_done: true).group_by_month.count
     @ttevents_hours_by_month = Ttevent.where(is_done: true).group_by_month.sum(:duration)
-    @ttevents_by_day = Ttevent.where(is_done: true).group_by_day.count
-    @ttevents_hours_by_day = Ttevent.where(is_done: true).group_by_day.sum(:duration)
-    gon.ttevents = @ttevents_by_month.map {|t| {year: t[0][0], month: t[0][1], count: t[1]}}
+  end
+
+  def stats_by_day
+    @ttevents_by_day = Ttevent.where(is_done: true).order("start_time DESC").group_by_day.limit(10).count
+    @ttevents_hours_by_day = Ttevent.where(is_done: true).order("start_time DESC").group_by_day.limit(10).sum(:duration)
+  end
+
+  private
+  def set_unreported_ttevents_count
+    @unreported_ttevents_count = Ttevent.where('is_done = ? AND end_time < ?',false, Time.now).count
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ get 'ttevents/issue_lists' => 'ttevents#issue_lists'
 resources :ttevents
 patch 'ttevents/:id/with_issue' => 'ttevents#update_with_issue', as: :ttevent_with_issue
 
+get 'ttstatistics/stats_by_month' => 'ttstatistics#stats_by_month'
+get 'ttstatistics/stats_by_day' => 'ttstatistics#stats_by_day'
 resources :ttstatistics
