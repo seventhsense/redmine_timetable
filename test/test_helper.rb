3404c11d746d61e0175a36ecf797dc1b772b2c4d
seventhsense <seventh@lh4l.com>
2015-05-09 14:58:43 +0900

    add unit test for ttevent

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,17 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 # require 'simplecov'
-# require 'minitest/hell'
+require 'minitest/hell'
 require 'minitest/reporters'
-require 'database_cleaner'
+# require 'database_cleaner'
 Minitest::Reporters.use!
 
-DatabaseCleaner.strategy = :truncation
+# DatabaseCleaner.strategy = :truncation
 # DatabaseCleaner.clean
 
 include MiniTest::ActiveRecordAssertions
 # SimpleCov.start do
   # root(SimpleCov.root + '/plugins/redmine_timetable')
-  # add_group "Models", "app/models"
-  # # coverage_dir(SimpleCov.root + '/coverage')
+  # add_group "Models", "plugins/redmine_timetable/app/models"
+  # coverage_dir(SimpleCov.root + '/coverage')
 # end
--- test/unit/ttevent_test.rb
+++ test/unit/ttevent_test.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class TteventTest < ActiveSupport::TestCase
-  fixtures :projects
+  fixtures :projects, :issues, :users
+  # fixtures :ttevents
   # def setup
     # puts 'before'
   # end
@@ -12,14 +13,62 @@ class TteventTest < ActiveSupport::TestCase
   # def after_tests
     # puts 'after tests'
   # end
-  def test_load_fixture
-    @project = projects(:projects_001)
-    # assert_same 1, @project.id
-    assert { @project.id == 1 }
-  end
+  # def test_load_fixture
+    # @project = projects(:projects_001)
+    # # assert_same 1, @project.id
+    # assert { @project.id == 1 }
+  # end
 
   def test_issue_has_many_ttevents
     assert_association Issue, :has_many, :ttevents
     assert_association Ttevent, :belongs_to, :issue
+  end
+
+  def test_save
+    set_one_ttevent_sample
+    assert { @ttevent.is_done == false }
+  end
+
+  def test_done
+    set_one_ttevent_sample
+    @done_count = Ttevent.done.count
+    assert { @done_count == 0 }
+    @undone_count = Ttevent.undone.count
+    assert { @undone_count == 1}
+  end
+
+  def test_duration
+    set_one_ttevent_sample
+    assert { @ttevent.duration == 0.5 }
+  end
+
+  def test_color
+    # due_date is nil
+    set_one_ttevent_sample(:issues_002)
+    assert { @ttevent.color == "#da3aad" }
+    # due_date is 10 days after
+    set_one_ttevent_sample
+    assert { @ttevent.color == "#3aad87"}
+    # due_date is 5 days ago
+    set_one_ttevent_sample(:issues_003)
+    assert { @ttevent.color == '#da3a3a' }
+    # due_date is 1 day after
+    set_one_ttevent_sample(:issues_006)
+    assert { @ttevent.color == '#da873a' }
+  end
+
+  private
+  def set_one_ttevent_sample(issue_no= :issues_001)
+    @user = users(:users_002)   
+    @issue = issues(issue_no)
+    start_time = Time.now
+    end_time = start_time.since(30.minutes)
+    @ttevent = Ttevent.new(
+      user_id: @user.id,
+      issue_id: @issue.id,
+      start_time: start_time,
+      end_time: end_time
+    )
+    @ttevent.save
   end
 end
