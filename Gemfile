d7b3aec1666ba40e09ee8f41b5a0537acadef227
seventhsense <seventh@lh4l.com>
2015-05-09 12:02:33 +0900

    add test case

--- Gemfile
+++ Gemfile
@@ -3,3 +3,10 @@ source 'https://rubygems.org'
 gem 'coffee-rails', '4.1.0'
 gem 'gon', '5.2.3'
 gem 'holiday_jp'
+
+group :test do
+  gem 'database_cleaner'
+  gem 'minitest-reporters', '1.0.14'
+  gem 'minitest-ar-assertions', "0.1.1", :require => 'minitest_activerecord_assertions'
+  gem "minitest-power_assert", "0.0.3"    
+end
--- test/unit/ttevent_test.rb
+++ test/unit/ttevent_test.rb
@@ -1,9 +1,25 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class TteventTest < ActiveSupport::TestCase
+  fixtures :projects
+  # def setup
+    # puts 'before'
+  # end
+  # def teardown
+    # puts 'after'
+  # end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  # def after_tests
+    # puts 'after tests'
+  # end
+  def test_load_fixture
+    @project = projects(:projects_001)
+    # assert_same 1, @project.id
+    assert { @project.id == 1 }
+  end
+
+  def test_issue_has_many_ttevents
+    assert_association Issue, :has_many, :ttevents
+    assert_association Ttevent, :belongs_to, :issue
   end
 end
