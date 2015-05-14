ca66531cde05a770d48c5af3b9f583086ce989dc
seventhsense <seventh@scimpr.com>
2015-05-14 18:58:46 +0900

    add fixture sample

--- test/functional/ttstatistics_controller_test.rb
+++ test/functional/ttstatistics_controller_test.rb
@@ -3,12 +3,15 @@ require File.expand_path('../../test_helper', __FILE__)
 class TtstatisticsControllerTest < ActionController::TestCase
   fixtures :users, :issues, :ttevents
   def setup
-    @request.session[:user_id] = 1
+    @request.session[:user_id] = 2 
     Setting.default_language = 'ja'
   end
+
   def test_index
     get :index
     assert_response :success
+    p assigns(:current_user)
+    p assigns(:issues_assigned_count)
   end
 
   def test_stats_by_month
